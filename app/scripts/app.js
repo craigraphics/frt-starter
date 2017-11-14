904ab41e7d9a7c18046287f5c1668c8b3c5f855e
William Craig Zafra <william.craig@globant.com>
2017-11-14 11:10:07 -0600

    added hero slider
    fixed social media icons

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
   $.getScript("//platform.twitter.com/widgets.js");
+  var slider = $('.hero-slider');
+
+  slider.slick({
+    dots: true,
+    infinite: false,
+    autoplay: true,
+    speed: 800,
+    autoplaySpeed: 5000,
+    arrows: false,
+    fade: false,
+    cssEase: 'linear',
+    pauseOnHover: false
+  });
 });
