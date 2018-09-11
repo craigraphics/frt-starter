548efc9850a557cb9be8bd0d291178dc9ff1dd57
craigraphics <craigraphics@gmail.com>
2018-09-11 10:23:40 -0500

    add source maps

--- webpack.config.js
+++ webpack.config.js
@@ -15,59 +15,76 @@ var extractCssPlugin = new ExtractTextPlugin({
 });
 
 module.exports = {
-    entry: './src/js/app.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-        // publicPath: '/dist'
+  devtool: "source-map",
+  entry: './src/js/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    // publicPath: '/dist'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      use: [{
+        loader: 'babel-loader',
+        options: {
+          presets: ['env']
+        }
+      }]
+    }, {
+      test: /\.css$/,
+      loader: extractCssPlugin.extract({
+        loader: 'css-loader?modules&importLoaders=1&localIdentName=[local]_[hash:base64:6]',
+      }),
+    }, {
+      test: /\.scss$/,
+      use: extractScssPlugin.extract({
+        use: [
+            {
+              loader: 'css-loader',
+              options: { sourceMap: true }
+            }, {
+              loader: 'postcss-loader',
+              options: {
+                plugins: () => [require('autoprefixer')({
+                  'browsers': ['> 1%', 'last 2 versions']
+                })],
+              }
+            }, {
+              loader: 'sass-loader',
+              options: { sourceMap: true }
+
+            }
+          ]
+      })
+    }, {
+      test: /\.html$/,
+      use: ['html-loader?interpolate']
+    }, {
+      test: /\.(jpg|png)$/,
+      use: [{
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: 'img/',
+          publicPath: 'img/'
+        }
+      }]
     },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            use: [{
-                loader: 'babel-loader',
-                options: {
-                    presets: ['es2015']
-                }
-            }]
-        }, { // regular css files
-          test: /\.css$/,
-          loader: extractCssPlugin.extract({
-            loader: 'css-loader?importLoaders=1',
-          }),
-        }, {
-            test: /\.scss$/,
-            use: extractScssPlugin.extract({
-                use: ['css-loader', 'sass-loader']
-            })
-        }, {
-            test: /\.html$/,
-            use: ['html-loader?interpolate']
-        }, {
-            test: /\.(jpg|png)$/,
-            use: [{
-                loader: 'file-loader',
-                options: {
-                    name: '[name].[ext]',
-                    outputPath: 'img/',
-                    publicPath: 'img/'
-                }
-            }]
-        },
-        {
-                     test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-                     use: `url-loader?limit=10000&mimetype=image/svg+xml&${fileAsset}`
-                 }]
-    },
-    plugins: [
-        extractCssPlugin,
-        extractScssPlugin,
-        new HtmlWebpackPlugin({
-            template: 'src/index.html'
-        }),
-        new CleanWebpackPlugin(['dist']),
-        new CopyWebpackPlugin([
-            { from: 'shared', to: 'shared' }
-        ])
-    ]
+    {
+      test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+      use: `url-loader?limit=10000&mimetype=image/svg+xml&${fileAsset}`
+    }]
+  },
+  plugins: [
+    extractCssPlugin,
+    extractScssPlugin,
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new CleanWebpackPlugin(['dist']),
+    new CopyWebpackPlugin([
+      { from: 'shared', to: 'shared' }
+    ])
+  ]
 };
